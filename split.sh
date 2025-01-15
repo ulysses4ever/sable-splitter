049fc42e3f0b8dd3ce458f8540ec52b8a3e9df45
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2025-01-15 11:48:57 -0500

    split.sh: generate header with the SpMV kernel and include it into all foo$i.c
    Also fork-join compilation of individual foo$i.c files.

--- split.sh
+++ split.sh
@@ -19,7 +19,7 @@ FILE=$2
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 echo "0. Restore original"
-cp $FILE.orig $FILE
+cp $FILE.orig.a $FILE 2>/dev/null || echo "No original"
 
 echo '1. Extract the original foo-calls in `body.ins` and put foo$i-calls instead'
 runhaskell "$SCRIPT_DIR/replace_foo.hs" $N $FILE
@@ -29,11 +29,27 @@ split -n r/$N body.ins foo --numeric-suffixes=10 --additional-suffix=".c"
 
 echo "3. Patch the foo\$i.c: add funciton header and footer"
 for ((i=10; i<=$N1; i++)); do
-    sed -i "1i void foo$i(float *x, float *y, float *val) {" "foo$i.c"
-#    sed -i "1i #include \"foo.h\"\nvoid foo$i(float *x, float *y, float *val) {" "foo$i.c"
+#    sed -i "1i void foo$i(float *x, float *y, float *val) {" "foo$i.c"
+    sed -i "1i #include \"foo.h\"\nvoid foo$i(float *x, float *y, float *val) {" "foo$i.c"
     echo "}" >> "foo$i.c"
 done
 
+echo "4. Create foo.h with the SpMV kernel (function foo)"
+echo "
+inline
+void foo(float *y, const float* x, const float* val, int i_start, int i_end, int j_start, int j_end, int val_offset) {
+    for (int j = j_start; j < j_end; j++) {
+  for (int i = i_start; i < i_end; i++) {
+      y[i] += ((&val[val_offset])[(((j-j_start)*(i_end-i_start)) + (i-i_start))] * x[j]);
+    }
+  }
+}
+" > foo.h
+
 echo "5. Compiling and linking the result"
-gcc -c -O3 -Wno-implicit-function-declaration $FILE ./foo*.c
+for f in ./foo*.c; do
+    gcc -c -O3 $f &
+done
+wait
+gcc -c -O3 -Wno-implicit-function-declaration $FILE
 gcc ${FILE%.c}.o ./foo*.o
